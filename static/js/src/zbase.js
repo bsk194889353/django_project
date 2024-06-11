d1ec821f4efbf2864992543c4ebb284c3c8e6db7
sk b <16634272250@163.com>
2024-06-11 21:24:23 +0800

    write register

--- static/js/src/zbase.js
+++ static/js/src/zbase.js
@@ -3,8 +3,8 @@ export class AcGame {
         this.acwingOS = acwingOS;
         this.id = id;
         this.$ac_game = $('#'+id);
-        this.settings = new Settings(this);
         this.menu = new AcGameMenu(this);
+        this.settings = new Settings(this);
         this.playground = new AcGamePlayground(this);
         this.start();
     }
